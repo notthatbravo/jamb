b7daf40957b7e4a93b29f1adbb1c7d9a2f4911cb
Bravo <adeolaisrael424@gmail.com>
2021-07-10 15:32:07 +0100

    working on news section

---
+++ assets/scripts/news.js
@@ -0,0 +1,16 @@
+const card = document.querySelector('.news-card');
+const openCardBtn = document.querySelector('#read-more-btn');
+const closeCardBtn = document.querySelector('#close-btn');
+
+openCardBtn.addEventListener('click', openCard);
+closeCardBtn.addEventListener('click', closeCard);
+
+function openCard() {
+    card.classList.toggle('opened-news-card');
+    document.body.style.overflow = 'hidden';
+}
+
+function closeCard() {
+    card.classList.toggle('opened-news-card');
+    document.body.style.overflow = 'auto';
+}
