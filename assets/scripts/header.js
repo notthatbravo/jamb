8776d61d9f045a748415dab17a48814205552a9e
Bravo <adeolaisrael424@gmail.com>
2021-07-18 23:04:41 +0100

    started working on "more links" inside the sidebar

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -1,3 +1,4 @@
+// Toggling the main nav
 const hamburgerIcon = document.querySelector('#hamburger-menu-container');
 const sidebar = document.querySelector('nav');
 const line1 = document.querySelector('#line-1');
@@ -13,3 +14,30 @@ function toggleMenu() {
     line2.classList.toggle('active');
     line3.classList.toggle('active');
 }
+
+
+
+// Toggling sub-links inside the nav
+
+// The containers
+const subAboutLinks = document.querySelector('.about-links');
+const subQuickLinks = document.querySelector('.quick-links');
+const subNewsLinks = document.querySelector('.news-links');
+const subStatsLinks = document.querySelector('.stats-links');
+
+// The togglers
+const subAboutToggler = document.querySelector('#about-toggler');
+const subQuickLinksToggler = document.querySelector('#quick-links-toggler');
+const subNewsToggler = document.querySelector('#news-toggler');
+const subStatsToggler = document.querySelector('#stats-toggler');
+
+// Making shit work
+// Need to refactor this thing to make use of loops and dictionaries / objects
+subAboutToggler.addEventListener('click', function() {
+    sidebar.style.background = "red";
+});
+
+
+// subQuickLinksToggler
+// subNewsToggler
+// subStatsToggler
