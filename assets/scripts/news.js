c5d39514ad4ad124a49a13a37c422c168213ae17
Bravo <adeolaisrael424@gmail.com>
2021-07-24 22:36:42 +0100

    fixed the news section of hero page kpk

--- assets/scripts/news.js
+++ assets/scripts/news.js
@@ -1,14 +1,39 @@
-const card = document.querySelectorAll('.news-card');
-const openCardBtn = document.querySelectorAll('#read-more-btn');
-const closeCardBtn = document.querySelectorAll('#close-btn');
+// Cards
+const importantNoticeCard = document.querySelector('#important-notice-card');
+const communiqueCard = document.querySelector('#communique-card');
+const disclaimerCard = document.querySelector('#disclaimer-card');
 
-openCardBtn.forEach(el => el.addEventListener('click', openCard));
-closeCardBtn.forEach(el => el.addEventListener('click', closeCard));
 
-function openCard() {
-    card.forEach(el => el.classList.toggle('opened-news-card'));
+// Togglers
+const importantNoticeToggler = document.querySelector('#important-notice-toggler');
+const communiqueToggler = document.querySelector('#communique-toggler');
+const disclaimerToggler = document.querySelector('#disclaimer-toggler');
+
+// Untogglers (if that's an actual word)
+const importantNoticeUntoggler = document.querySelector('#important-notice-untoggler');
+const communiqueUntoggler = document.querySelector('#communique-untoggler');
+const disclaimerUntoggler = document.querySelector('#disclaimer-untoggler');
+
+
+// Making shit work
+function toggleActiveCard(toggler, card) {
+    toggler.addEventListener('click', function() {
+        card.classList.toggle('opened-news-card');
+    })
 }
 
-function closeCard() {
-    card.forEach(el => el.classList.toggle('opened-news-card'));
+function untoggleActiveCard(untoggler, card) {
+    untoggler.addEventListener('click', function() {
+        card.classList.toggle('opened-news-card');
+    })
 }
+
+// Opening the card
+toggleActiveCard(importantNoticeToggler, importantNoticeCard);
+toggleActiveCard(communiqueToggler, communiqueCard);
+toggleActiveCard(disclaimerToggler, disclaimerCard);
+
+// Closing the card
+untoggleActiveCard(importantNoticeUntoggler, importantNoticeCard);
+untoggleActiveCard(communiqueUntoggler, communiqueCard);
+untoggleActiveCard(disclaimerUntoggler, disclaimerCard);
