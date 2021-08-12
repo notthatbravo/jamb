4bf01a19af60783cb1f58650460fb9305410f0aa
Bravo <adeolaisrael424@gmail.com>
2021-08-12 22:11:54 +0100

    added filtering faqs feature

--- assets/scripts/support.js
+++ assets/scripts/support.js
@@ -1,3 +1,27 @@
+//Filtering FAQs
+summary = document.getElementsByTagName('summary');
+filterFaqsInput = document.getElementById('faqs-input');
+
+filterFaqsInput.addEventListener('keyup', function() {
+    filter = filterFaqsInput.value.toLowerCase();
+
+    for (i = 0; i < summary.length; i++) {
+        detailsContainer = summary[i].parentElement;
+
+        summaryContent = summary[i].textContent || summary[i].innerText;
+        summaryContent = summaryContent.toLowerCase();
+
+        if (summaryContent.indexOf(filter) == -1) {
+            detailsContainer.style.display = 'none'
+        } else {
+            detailsContainer.style.display = 'block';
+        }
+    }
+
+})
+
+console.log('How do I change my choice of institutions and/or courses?'.indexOf('choice'))
+
 //Filtering Office Locations
 state = document.getElementsByTagName('h4');
 filterStatesInput = document.getElementById('filterStatesInput');
